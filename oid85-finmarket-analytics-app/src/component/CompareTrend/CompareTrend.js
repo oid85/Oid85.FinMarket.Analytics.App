ad7fdc8c11637bf0247d55fe91d9446f8ffba777
oid85 <oid85@mail.ru>
2026-02-21 23:08:27 +0700

    .

--- oid85-finmarket-analytics-app/src/component/CompareTrend/CompareTrend.js
+++ oid85-finmarket-analytics-app/src/component/CompareTrend/CompareTrend.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { sagaCompareTrend } from '../../redux/actions/compareTrendActions'
-import { sagaInstrumentList, sagaInstrumentSelect, fetchCurrentInstrument } from '../../redux/actions/instrumentActions'
-import { InstrumentList } from '../Instrument/InstrumentList'
 import Loader from '../Loader/Loader'
 import { CompareTrendDiagram } from './CompareTrendDiagram'
 import './styles.css'
@@ -12,30 +10,18 @@ export const CompareTrend = () => {
     const dispatch = useDispatch()
     const loading = useSelector(state => state.app.loading)
     const compareTrendData = useSelector(state => state.compareTrend.compareTrendData)
-    const instrumentListData = useSelector(state => state.instrument.instrumentListData)
 
     useEffect(() => {
         dispatch(sagaCompareTrend())
-    }, [])
-
-    useEffect(() => {
-        dispatch(sagaCompareTrend())
-    }, [instrumentListData])
-
-    useEffect(() => {
-        dispatch(sagaInstrumentList())
     }, [])
 
     return (
         <React.Fragment>
         {
-            !compareTrendData.result || !instrumentListData.result || loading
+            !compareTrendData.result || loading
             ? <Loader/>
             :
-            <div className='horizontal-container'>
-                <div className='instrument-list-container'>
-                    <InstrumentList instruments = {instrumentListData.result.instruments} />
-                </div>                
+            <div className='horizontal-container'>             
                 <div className='compare-trend-container border-style'>
                     <CompareTrendDiagram series={compareTrendData.result.series}/>
                 </div> 
--- oid85-finmarket-analytics-app/src/component/TrendDynamic/TrendDynamic.js
+++ oid85-finmarket-analytics-app/src/component/TrendDynamic/TrendDynamic.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { sagaTrendDynamic } from '../../redux/actions/trendDynamicActions'
-import { sagaInstrumentList, sagaInstrumentSelect, fetchCurrentInstrument } from '../../redux/actions/instrumentActions'
-import { InstrumentList } from '../Instrument/InstrumentList'
 import Loader from '../Loader/Loader'
 import './styles.css'
 import { TrendDynamicData } from './TrendDynamicData'
@@ -13,30 +11,18 @@ export const TrendDynamic = () => {
     const dispatch = useDispatch()
     const loading = useSelector(state => state.app.loading)
     const trendDynamicData = useSelector(state => state.trendDynamic.trendDynamicData)
-    const instrumentListData = useSelector(state => state.instrument.instrumentListData)
 
     useEffect(() => {
         dispatch(sagaTrendDynamic())
-    }, [])
-
-    useEffect(() => {
-        dispatch(sagaTrendDynamic())
-    }, [instrumentListData])
-
-    useEffect(() => {
-        dispatch(sagaInstrumentList())
     }, [])
 
     return (
         <React.Fragment>
         {
-            !trendDynamicData.result || !instrumentListData.result || loading
+            !trendDynamicData.result || loading
             ? <Loader/>
             :
-            <div className='horizontal-container'>
-                <div className='instrument-list-container'>
-                    <InstrumentList instruments = {instrumentListData.result.instruments} />
-                </div>                
+            <div className='horizontal-container'>             
                 <div className='trend-dynamic-container border-style'>
                     <TrendDynamicDates dates = {trendDynamicData.result.dates} />
                     <h6>Индексы</h6>
