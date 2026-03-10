e88e3009d83a2aef60e10ac7e830130c1019cbb8
oid85 <oid85@mail.ru>
2026-03-11 00:25:40 +0700

    .

--- oid85-finmarket-analytics-app/src/component/ClosePriceDiagram/ClosePriceDiagramComponent.js
+++ oid85-finmarket-analytics-app/src/component/ClosePriceDiagram/ClosePriceDiagramComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AreaChart, Area, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './styles.css'
+import {CONSTANTS} from '../../constants'
 
 export const ClosePriceDiagramComponent = ({ key, ticker, name, inPortfolio, data }) => {
     return (
@@ -11,7 +12,7 @@ export const ClosePriceDiagramComponent = ({ key, ticker, name, inPortfolio, dat
                 <CartesianGrid strokeDasharray="3 3" />
                 <YAxis type="number" domain={['auto', 'auto']} />
                 <Tooltip />
-                <Area type="monotone" dataKey="value" strokeWidth={3} stroke="#8884d8" fill="#8884d8" />
+                <Area type="monotone" dataKey="value" strokeWidth={3} stroke={CONSTANTS.COLOR_GREEN} fill={CONSTANTS.COLOR_LIGHTGREEN} />
             </AreaChart>
         </div>
     )
