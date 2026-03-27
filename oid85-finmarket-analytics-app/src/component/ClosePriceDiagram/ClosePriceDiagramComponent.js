27ef2249af4d25bc3159218621f4da7ce580bbae
oid85 <oid85@mail.ru>
2026-03-27 18:55:24 +0700

    .

--- oid85-finmarket-analytics-app/src/component/ClosePriceDiagram/ClosePriceDiagramComponent.js
+++ oid85-finmarket-analytics-app/src/component/ClosePriceDiagram/ClosePriceDiagramComponent.js
@@ -5,7 +5,8 @@ import {CONSTANTS} from '../../constants'
 
 export const strokeColor = (value) => {
     if (!value) { return CONSTANTS.COLOR_STATEBLUE }
-    if (value == 'ТРЕНД') { return CONSTANTS.COLOR_GREEN }
+    if (value == 'ТРЕНД ВВЕРХ') { return CONSTANTS.COLOR_GREEN }
+    if (value == 'ТРЕНД ВНИЗ') { return CONSTANTS.COLOR_RED }
     if (value == 'НЕТ ТРЕНДА') { return CONSTANTS.COLOR_DARKSLATEGRAY }
 
     return CONSTANTS.COLOR_STATEBLUE
@@ -13,7 +14,8 @@ export const strokeColor = (value) => {
 
 export const fillColor = (value) => {
     if (!value) { return CONSTANTS.COLOR_STATEBLUE }
-    if (value == 'ТРЕНД') { return CONSTANTS.COLOR_LIGHTGREEN }
+    if (value == 'ТРЕНД ВВЕРХ') { return CONSTANTS.COLOR_LIGHTGREEN }
+    if (value == 'ТРЕНД ВНИЗ') { return CONSTANTS.COLOR_LIGHTRED }
     if (value == 'НЕТ ТРЕНДА') { return CONSTANTS.COLOR_LIGHTYELLOW }
 
     return CONSTANTS.COLOR_STATEBLUE
--- oid85-finmarket-analytics-app/src/component/PortfolioPositionList/colorHelper.js
+++ oid85-finmarket-analytics-app/src/component/PortfolioPositionList/colorHelper.js
@@ -2,11 +2,9 @@ import { CONSTANTS } from "../../constants"
 
 export const rowColor = (value) => {
     if (!value) { return CONSTANTS.COLOR_WHITE }
-    if (value == 'ТРЕНД') { return CONSTANTS.COLOR_GREEN }
-    if (value == 'СИЛЬНЫЙ ТРЕНД') { return CONSTANTS.COLOR_GREEN }
+    if (value == 'ТРЕНД ВВЕРХ') { return CONSTANTS.COLOR_GREEN }
     if (value == 'НЕТ ТРЕНДА') { return CONSTANTS.COLOR_YELLOW }
-    if (value == 'НЕ ОПРЕДЕЛЕН') { return CONSTANTS.COLOR_YELLOW }
-    if (value == 'СЛОМ ТРЕНДА') { return CONSTANTS.COLOR_RED }
+    if (value == 'ТРЕНД ВНИЗ') { return CONSTANTS.COLOR_RED }
 
     return CONSTANTS.COLOR_WHITE
 }
