68aa23b67a00ab91688dbf6d6a3e0c881487e7b4
oid85 <oid85@mail.ru>
2026-07-31 15:46:14 +0700

    .

--- oid85-finmarket-analytics-app/src/redux/reducers/algoReducer.js
+++ oid85-finmarket-analytics-app/src/redux/reducers/algoReducer.js
@@ -11,7 +11,7 @@ const initialState = {
     portfolioMonitorData: {},
     portfolioBacktestData: {},
     portfolioOptimizationData: {},
-    currentPortfolio: { name: '', description: '' }    
+    currentPortfolio: { name: 'Trend', description: 'Портфель Trend (Tinkoff Женя)' }    
 }
 
 export const algoReducer = (state = initialState, action) => {
