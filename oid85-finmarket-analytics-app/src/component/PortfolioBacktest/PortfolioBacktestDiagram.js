22e8573218dac054df82bd5712dcd7bbbfc252ae
oid85 <oid85@mail.ru>
2026-09-14 22:21:57 +0700

    .

--- oid85-finmarket-analytics-app/src/component/PortfolioBacktest/PortfolioBacktestDiagram.js
+++ oid85-finmarket-analytics-app/src/component/PortfolioBacktest/PortfolioBacktestDiagram.js
@@ -23,7 +23,7 @@ export const PortfolioBacktestDiagram = ({series}) => {
                         key={s.name} 
                         stroke={s.color}
                         fill={s.colorFill}
-                        strokeWidth={5}
+                        strokeWidth={3}
                         dot={false}                        
                         />
                 ))}
